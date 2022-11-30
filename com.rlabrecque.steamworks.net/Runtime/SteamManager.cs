c4ce9d90cfbbe3d731bc1e6cc111555263c6797d
jclemente-statespace <jason@statespacelabs.com>
2022-11-30 11:12:28 -0600

    Removing restart on steam

--- com.rlabrecque.steamworks.net/Runtime/SteamManager.cs
+++ com.rlabrecque.steamworks.net/Runtime/SteamManager.cs
@@ -126,10 +126,12 @@ public class SteamManager : MonoBehaviour {
 			// Once you get a Steam AppID assigned by Valve, you need to replace AppId_t.Invalid with it and
 			// remove steam_appid.txt from the game depot. eg: "(AppId_t)480" or "new AppId_t(480)".
 			// See the Valve documentation for more information: https://partner.steamgames.com/doc/sdk/api#initialization_and_shutdown
+#if !EGS
 			if (SteamAPI.RestartAppIfNecessary(AppId_t.Invalid)) {
 				Application.Quit();
 				return;
 			}
+#endif
 		}
 		catch (System.DllNotFoundException e) { // We catch this exception here, as it will be the first occurrence of it.
 			currentError = "[Steamworks.NET] Could not load [lib]steam_api.dll/so/dylib. It's likely not in the correct location. Refer to the README for more details.\n" + e;
